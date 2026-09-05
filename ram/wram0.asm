ad483abaae4f320da84723c41ccbba319f357455
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-09-05 12:56:38 -0400

    Limit the Pokégear phone scrolling to your number of contacts
    Fixes #1516

--- ram/wram0.asm
+++ ram/wram0.asm
@@ -724,6 +724,7 @@ wPokegearRadioChannelAddr:: dw
 wPokegearRadioMusicPlaying:: db
 wPokegearNumberBuffer:: db
 wPokegearMapCursorSpawnpoint:: db
+wPokegearPhoneMaxContact:: db
 
 
 SECTION UNION "Misc 404", WRAM0
