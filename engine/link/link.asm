48ce125d128d8696eb18f85230445be0907f2763
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-09-05 16:25:49 -0400

    Remove unused trademon caught data (was used for OT gender)

--- engine/link/link.asm
+++ engine/link/link.asm
@@ -1485,9 +1485,6 @@ LinkTrade:
 .got_tradeparty_species
 	ld [wPlayerTrademonSpecies], a
 	push af
-; caught data
-	xor a
-	ld [wPlayerTrademonCaughtData], a
 ; OT name
 	ld a, [wCurTradePartyMon]
 	ld hl, wPartyMonOTs
@@ -1560,9 +1557,6 @@ LinkTrade:
 	ld [wOTTrademonDVs + 1], a
 	ld a, [hl]
 	ld [wOTTrademonDVs + 2], a
-; caught data
-	xor a
-	ld [wOTTrademonCaughtData], a
 
 	ld a, [wCurTradePartyMon]
 	ld [wCurPartyMon], a
